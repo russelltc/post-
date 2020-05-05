0f717aaada271206f5d9c6e33d8930e26130a388
russelltc <russelltc@lopers.unk.edu>
2020-05-04 20:30:51 -0500

    post hw

---
+++ src/main/java/com/cybr406/post/configuration/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.cybr406.post.configuration;
+
+import com.cybr406.post.security.user.AccountAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    H2SecurityConfigurer h2SecurityConfigurer;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth, AccountAuthenticationProvider authprovider){
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        h2SecurityConfigurer.configure(http);
+        http
+                .authorizeRequests()
+                .mvcMatchers(HttpMethod.GET,"/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .httpBasic();
+    }
+
+
+}
